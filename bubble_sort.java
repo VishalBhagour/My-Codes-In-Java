7b0d5f7599493b09ec1db4ff489760dcfe722c3d
VishalBhagour <vishalbhagour7@gmail.com>
2025-08-12 15:06:26 +0530

    bubble sort code

---
+++ bubble_sort.java
@@ -0,0 +1,20 @@
+public class bubble_sort {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 10, 40, 50, 60, 70, 80};
+        int n = arr.length;
+        int temp = 0;
+        for(int i=0; i<n; i++){
+            for(int j=1; j<(n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+}
